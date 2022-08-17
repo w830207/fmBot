cf95fce2daa11960b3063e4b18028f48fa02f673
w830207 <44136372+w830207@users.noreply.github.com>
2022-08-17 13:35:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ updater.idle()
 
 if __name__ == "__main__":
     # Running server
-    port = int(os.environ.get('PORT', 27017))
+    port = int(os.environ.get('PORT', 50000))
     app.run(host='0.0.0.0', port=port)
